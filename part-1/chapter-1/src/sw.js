6f153fe161457e291391885c70a0c0669aff6758
yuye <659672626@qq.com>
2023-08-27 23:56:54 +0800

    添加pwa、 manifest.json、serve worker监听开启

---
+++ part-1/chapter-1/src/sw.js
@@ -0,0 +1,28 @@
+
+// serviceWorker 监听 install事件，回调中缓存所需文件
+self.addEventListener('install', function(event) {
+    // event.waitUntil(
+    //     caches.open('v1').then(function(cache) {
+    //         return cache.addAll([
+    //             './',
+    //             './index.html',
+    //             './index.js',
+    //             './index.css',
+    //             './assets/images/logo.png'
+    //         ])
+    //     })
+    // )
+})
+
+// 拦截所有请求事件
+// 如果缓存中已经有请求的数据就直接用缓存，否则去请求数据
+self.addEventListener('fetch', function(event) {
+    // event.respondWith(
+    //     caches.match(event.request).then(function(response) {
+    //         if (response) {
+    //             return response
+    //         }
+    //         return fetch(event.request)
+    //     })
+    // )
+})
