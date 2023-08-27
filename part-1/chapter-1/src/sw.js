945b8d75dda1148237237b2b05ec99481b89b66f
yuye <659672626@qq.com>
2023-08-28 00:01:11 +0800

    no message

--- part-1/chapter-1/src/sw.js
+++ part-1/chapter-1/src/sw.js
@@ -1,6 +1,7 @@
 
 // serviceWorker 监听 install事件，回调中缓存所需文件
 self.addEventListener('install', function(event) {
+    console.log('service worker install watch监听');
     // event.waitUntil(
     //     caches.open('v1').then(function(cache) {
     //         return cache.addAll([
@@ -17,6 +18,7 @@ self.addEventListener('install', function(event) {
 // 拦截所有请求事件
 // 如果缓存中已经有请求的数据就直接用缓存，否则去请求数据
 self.addEventListener('fetch', function(event) {
+    console.log('service worker fetch watch监听');
     // event.respondWith(
     //     caches.match(event.request).then(function(response) {
     //         if (response) {
